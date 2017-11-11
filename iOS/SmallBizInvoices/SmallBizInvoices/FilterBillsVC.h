d5a3854805bce6c5acccf3a71f7dbfe45ac03ab3
Jay Vachhani <jay.vachhani1@gmail.com>
2017-11-11 14:46:33 -0800

    Added SmallBizAppManager, FilterBillsVC and BillReportVC classes

---
+++ iOS/SmallBizInvoices/SmallBizInvoices/FilterBillsVC.h
@@ -0,0 +1,13 @@
+//
+//  FilterBillsVC.h
+//  SmallBizInvoices
+//
+//  Created by Jay Vachhani on 11/11/17.
+//  Copyright © 2017 Reema Bhalani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterBillsVC : UIViewController
+
+@end
