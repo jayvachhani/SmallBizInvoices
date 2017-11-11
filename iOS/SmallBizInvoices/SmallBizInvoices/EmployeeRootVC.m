8efb4d445c6b51d550a1f22c6f82de20141f16b3
Reema Bhalani <reema.bhalani@gmail.com>
2017-11-11 14:18:04 -0800

    Added EmployeeRootVC

---
+++ iOS/SmallBizInvoices/SmallBizInvoices/EmployeeRootVC.m
@@ -0,0 +1,37 @@
+//
+//  EmployeeRootVC.m
+//  SmallBizInvoices
+//
+//  Created by Reema Bhalani on 11/11/17.
+//  Copyright © 2017 Reema Bhalani. All rights reserved.
+//
+
+#import "EmployeeRootVC.h"
+
+@interface EmployeeRootVC ()
+
+@end
+
+@implementation EmployeeRootVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
